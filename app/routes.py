de326c922caf8ff3611d1e48cda096bc0e828d95
farid-sharifi <faridsharifi151@gmail.com>
2026-02-02 10:09:00 -0800

    first commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint,render_template
+from dotenv import load_dotenv
+import os
+
+main = Blueprint("main",__name__)
+load_dotenv()
+for_test = os.getenv("EXAMPLE_SECRET")
+
+@main.route("/")
+def home ():
+    return render_template("index.html",for_test=for_test)
+
+
+import time
+
+@main.route("/slow")
+def slow ():
+    time.sleep(20)
+    return "done"
